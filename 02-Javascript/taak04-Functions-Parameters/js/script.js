3702af14a0c1404b2df419c36c204de9eaae2e7f
Thomas Pos <thomas.pos@student.rocva.nl>
2021-04-15 11:45:10 +0200

    function gemaakt

---
+++ 02-Javascript/taak04-Functions-Parameters/js/script.js
@@ -0,0 +1,7 @@
+function getallen(Getal1, Getal2){
+    Getal1 = prompt("Voer het 1e getal in");
+    Getal2 = prompt("Voer het 2e getal in");
+    let som = Getal1 + Getal2;
+    return som;
+}
+console.log(getallen());
