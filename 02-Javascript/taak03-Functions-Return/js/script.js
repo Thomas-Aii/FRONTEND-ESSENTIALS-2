de467b392a5e675437d3c7a86b099a32b921eede
Thomas Pos <thomas.pos@student.rocva.nl>
2021-04-15 11:31:28 +0200

    functie gemaakt

--- 02-Javascript/taak03-Functions-Return/js/script.js
+++ 02-Javascript/taak03-Functions-Return/js/script.js
@@ -1,5 +1,6 @@
-function geefRandomGetalTerug(){                
-    
+function geefRandomGetalTerug(){                   
+    let randomGetal = Math.random();                
+     return Math.floor(Math.random() * 11);
 }
 
-console.log(geefRandomGetalTerug());    
+console.log(geefRandomGetalTerug()); 
