5577f50a0c238cd5a14e585dab4d4ff54c4cad75
Thomas Pos <thomas.pos@student.rocva.nl>
2021-04-15 10:23:30 +0200

    herhaling

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,9 @@
 let strNaam;
 let boolNaamKlopt;
+strNaam = prompt("Hoe heet je?");
+if(boolNaamKlopt = confirm("heet je echt " + strNaam +"?")){
+    alert("Welkom " + strNaam + "!");
+}
+else{
+    strNaam = prompt("Nogmaals hoe heet je?");
+}
