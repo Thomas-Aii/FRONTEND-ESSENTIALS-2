43e0a7cbced08f122e9c30d6d55ac8360add27c1
Thomas Pos <thomas.pos@student.rocva.nl>
2021-04-16 11:05:01 +0200

    arrays gemaakt

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,13 @@
+topFilms = ["Grown Ups", "Avengers Endgame", "The Hangover"];
+topDieren = ["Worlf", "Tijger", "pinguin"];
+topGames = ["Rocket leuge", "Hyper scape", "Apex legends"];
+
+console.log(topFilms[0]);
+console.log(topFilms[1]);
+console.log(topFilms[2]);
+console.log(topDieren[0]);
+console.log(topDieren[1]);
+console.log(topDieren[2]);
+console.log(topGames[0]);
+console.log(topGames[1]);
+console.log(topGames[2]);
