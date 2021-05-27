731698dc65a8b28cc831558c0f762111b4b82666
Thomas Pos <thomas.pos@student.rocva.nl>
2021-05-27 12:24:14 +0200

    opdracht gemaakt

--- 02-Javascript/taak13-Loops-Fibonacci/js/script.js
+++ 02-Javascript/taak13-Loops-Fibonacci/js/script.js
@@ -1,2 +1,13 @@
-var fibonacci = [0,1];
+var a,b,resultaat;
+
+a=0;
+b=1;
+resultaat=b;
+
+for (var i=1; i<11; i++){
+    resultaat=a+b;
+    a=b;
+    b=resultaat;
+    console.log(resultaat);
+}
 
