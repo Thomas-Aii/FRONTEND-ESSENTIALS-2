e71eeb7f15d8d5ff0bbcf6fe40655c7affa11ebd
Thomas Pos <thomas.pos@student.rocva.nl>
2021-05-28 10:21:09 +0200

    opdracht gemaakt

--- 02-Javascript/taak14-Loops-Strings/js/script.js
+++ 02-Javascript/taak14-Loops-Strings/js/script.js
@@ -1,9 +1,8 @@
+var str = prompt("wat is je zin?");
+
 function outputLetters(str){
     for (let i = 0; i < str.length; i++) {
-        const element = array[i];
-        
     }
-}
-
-var voorbeeldString = "Dit is een array van letters";
-console.log(voorbeeldString.length); // geeft terug: 
+} 
+console.log(str);
+outputLetters(str);
